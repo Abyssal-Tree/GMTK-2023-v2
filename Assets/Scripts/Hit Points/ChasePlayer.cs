25e97accc397a3a870fbf4a2a1e3ea15af8a87b3
Jorge Torres Vargas <abyssal.tree.123@gmail.com>
2023-08-07 16:19:14 -0600

    Initial commit

---
+++ Assets/Scripts/Hit Points/ChasePlayer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class ChasePlayer : MonoBehaviour
+{
+
+    [SerializeField] private float speed;
+    [SerializeField] private Transform root;
+
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponentInParent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        var playerPos = Player.Instance.transform.position;
+        var dir = playerPos - transform.position;
+        dir.Normalize();
+        root.right = dir;
+        rb.velocity = dir * speed;
+    }
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,20 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] private float duration;
+
+    [SerializeField] private TextMeshProUGUI timer;
+    private float startT;
+    private void Start()
+    {
+        startT = Time.time;
+    }
+
+    private void Update()
+    {
+        var t = duration - Time.time - startT;
+        timer.text = t <= 0 ? "Victory" : Mathf.RoundToInt(t).ToString();
+    }
+}
